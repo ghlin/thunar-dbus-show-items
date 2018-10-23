bbbe4316a6b1513e8579805b6fd9355ddcc9ad61
ghlin <2012.2.9.ghl@gmail.com>
2018-10-23 17:12:11 +0800

    add .service file; renew thunar dbus interface when needed

--- expose-filemanager1-show-items-method.py
+++ expose-filemanager1-show-items-method.py
@@ -4,8 +4,11 @@
 from gi.repository import GLib
 from urllib.parse import unquote
 
+import sys
+
 import dbus
 import dbus.service
+import dbus.exceptions
 import dbus.mainloop.glib
 
 def decompose_uri(uri):
@@ -19,18 +22,22 @@ def decompose_uri(uri):
     folder     = uri_part[colon+3:]
     return (unquote(folder), unquote(filename))
 
+def get_thunar_interface(bus):
+    thunar_bus_name = 'org.xfce.FileManager'
+    thunar_bus_path = '/org/xfce/FileManager'
+
+    thunar_object = bus.get_object(thunar_bus_name, thunar_bus_path)
+    thunar        = dbus.Interface(thunar_object, thunar_bus_name)
+
+    return thunar
+
 filemanager1_bus_name = 'org.freedesktop.FileManager1'
 filemanager1_bus_path = '/org/freedesktop/FileManager1'
 
 class FileManager1(dbus.service.Object):
     def __init__(self, session_bus, path):
         super().__init__(session_bus, path)
-
-        thunar_bus_name = 'org.xfce.FileManager'
-        thunar_bus_path = '/org/xfce/FileManager'
-
-        thunar_object = session_bus.get_object(thunar_bus_name, thunar_bus_path)
-        self.thunar   = dbus.Interface(thunar_object, thunar_bus_name)
+        self.thunar = get_thunar_interface(session_bus)
 
     @dbus.service.method(filemanager1_bus_name, in_signature='ass', out_signature='')
     def ShowItems(self, uris, startup_id):
@@ -42,8 +49,22 @@ class FileManager1(dbus.service.Object):
         for uri in uris:
             parts = decompose_uri(uri)
             print('Will open folder `{}\', and select file `{}\'.'.format(parts[0], parts[1]))
-
-            self.thunar.DisplayFolderAndSelect(parts[0], parts[1], '', '')
+            try:
+                self.thunar.DisplayFolderAndSelect(parts[0], parts[1], '', '')
+            except dbus.exceptions.DBusException as e:
+                print(e)
+                print("Caught DBusException, try renew thunar interface.")
+                self.thunar = get_thunar_interface(dbus.SessionBus())
+                try:
+                    self.thunar.DisplayFolderAndSelect(parts[0], parts[1], '', '')
+                except dbus.exceptions.DBusException as e:
+                    print(e)
+                    print("Another DBusException... good luck then.")
+                    sys.exit(-1)
+            except Exception as e:
+                print(e)
+                print("Don't know what to do then...")
+                sys.exit(-2)
 
         print('Done.')
 
